f4a5edaf081bad87e2919b7f6316398b9e736c17
火子 <yxyong@126.com>
2022-04-22 17:47:17 +0800

    优化公众号基础接口

--- src/HttpTrait.php
+++ src/HttpTrait.php
@@ -30,7 +30,7 @@ trait HttpTrait
       $body = $resp->getBody()->getContents();
       if ($resp->getStatusCode() == 200) {
         $content_type = $resp->getHeaderLine('Content-Type');
-        if (str_contains($content_type, 'application/json')) {
+        if (str_contains($content_type, 'application/json') || str_contains($content_type, 'text/plain')) {
           $json = json_decode($body, true);
           if (json_last_error() === JSON_ERROR_NONE) {
             if (isset($json['errcode']) && $json['errcode'] != 0) {
@@ -40,7 +40,7 @@ trait HttpTrait
             }
           }
         }
-        if (str_contains($content_type, 'application/xml')) {
+        if (str_contains($content_type, 'application/xml') || str_contains($content_type, 'text/plain')) {
           $result = $this->fromXml($body);
           if ($result) {
             // 请求失败
