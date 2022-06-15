1f195fc88279b64bb01fbc66363a5e07c6be39ea
火子 <yxyong@126.com>
2022-06-15 21:39:47 +0800

    用户数据结构

---
+++ src/User/PublicInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wanphp\Libray\Weixin\User;
+
+
+use Wanphp\Libray\Mysql\BaseInterface;
+
+interface  PublicInterface extends BaseInterface
+{
+  const TABLE_NAME = "weixin_users_public";
+}
